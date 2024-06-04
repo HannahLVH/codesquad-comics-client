7340b5f85f8b9098d4656897e0ba0d1c73bb7057
Hannah Lopez <hannahlopez@Hannahs-Mac-mini.local>
2024-06-03 23:20:26 -0400

    updated login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+    const [login, setLogin] = useState({
+        username: "",
+        password: "",
+    })
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
+        const {name, value} = e.target;
+        setLogin((prevSetLogin) => ({...prevSetLogin, [name]: value}))
+        console.log("Method running successfully")
+    }
+
+    console.log(login)
+
     return (
     <div>
         <div className="content-section">
@@ -9,15 +22,21 @@ const Login = () => {
                         <h1>LOGIN</h1>
                     </header>
                     <div className="form-container">
-                        <form action="#">
+                        <form action="#" onSubmit={handleLoginSubmit}>
                             <div className="form-fields">
                                 <div>
                                     <label for="username">Email Address: </label>
-                                    <input type="username" name="username" id="username" placeholder="Email"/>
+                                    <input type="text" name="username" id="username" placeholder="Email"
+                                    value={login.username}
+                                    onChange={handleLoginSubmit}
+                                    required/>
                                 </div>
                                 <div>
                                     <label for="password">Password: </label>
-                                    <input type="password" name="password" id="password" placeholder="Password" />
+                                    <input type="text" name="password" id="password" placeholder="Password"
+                                    value={login.password}
+                                    onChange={handleLoginSubmit}
+                                    required />
                                 </div>
                                 <div className="submit-button">
                                     <br/>
